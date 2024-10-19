0c8dd16c35bdc22f638ef2d3f795fcc54c59de78
Km.Van <kmvan.com@gmail.com>
2024-10-19 10:26:41 +0800

    Fixed filter issues in PHP 8.3.

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace InnStudio\SmsBao;
 
-include __DIR__ . '/vendor/autoload.php';
+include __DIR__.'/vendor/autoload.php';
 
-new App(__DIR__ . '/config.json');
+new App(__DIR__.'/config.json');
